d308e57950ca82113a1c6dcef5dcc4e4296522f6
josipcrnekovic <50879767+josipcrnekovic@users.noreply.github.com>
2019-06-03 21:57:17 +0200

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Settings set = new Settings();
+            set.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            HighScores high = new HighScores();
+            high.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Local loc = new Local();
+            loc.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Online online = new Online();
+            online.Show();
+            this.Hide();
+        }
+    }
+}
